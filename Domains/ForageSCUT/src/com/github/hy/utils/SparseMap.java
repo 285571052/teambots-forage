26c20c53a79735c53d06274004363d5e30b59d5c
285571052 <qhy285571052@gmail.com>
2019-01-10 15:27:54 +0800

    Add utils: sparse map

---
+++ Domains/ForageSCUT/src/com/github/hy/utils/SparseMap.java
@@ -0,0 +1,67 @@
+
+/*
+ * SparseMap.java
+ */
+package com.github.hy.utils;
+
+import java.util.*;
+
+public class SparseMap<T> implements Cloneable {
+    Map<Integer, Map<Integer, T>> elements = new HashMap<Integer, Map<Integer, T>>();
+
+    public void put(Integer x, Integer y, T v) {
+        Map<Integer, T> row = elements.get(x);
+        if (row == null) {
+            elements.put(x, new HashMap<Integer, T>());
+            row = elements.get(x);
+        }
+        row.put(y, v);
+    }
+
+    public T get(Integer x, Integer y) {
+        Map<Integer, T> row = elements.get(x);
+        if (row == null)
+            return null;
+        return row.get(y);
+    }
+
+    public SparseMap<T> clone() {
+        SparseMap<T> ret = new SparseMap<T>();
+
+        for (Map.Entry<Integer, Map<Integer, T>> row_entry : elements.entrySet()) {
+            int x = row_entry.getKey();
+            for (Map.Entry<Integer, T> v_entry : row_entry.getValue().entrySet()) {
+                int y = v_entry.getKey();
+                T v = v_entry.getValue();
+                ret.put(x, y, v);
+            }
+        }
+
+        return ret;
+    }
+
+    public static void main(String argv[]) {
+        SparseMap<Integer> sparse_map = new SparseMap<Integer>();
+        sparse_map.put(1, 2, 3);
+        System.out.println(sparse_map.get(1, 2));
+        sparse_map.put(1, 7, 8);
+        System.out.println(sparse_map.get(1, 7));
+        sparse_map.put(4, 5, 6);
+        System.out.println(sparse_map.get(4, 5));
+        sparse_map.put(1, 7, 9);
+        System.out.println(sparse_map.get(1, 7));
+        System.out.println(sparse_map.get(9, 9));
+        // output should be 3 8 6 9 null
+
+        SparseMap<Integer> sparse_map_clone = sparse_map.clone();
+        sparse_map.put(9, 9, 9);
+        System.out.println(sparse_map.get(9, 9));
+        sparse_map.put(1, 7, 8);
+        System.out.println(sparse_map.get(1, 7));
+        System.out.println(sparse_map_clone.get(1, 2));
+        System.out.println(sparse_map_clone.get(1, 7));
+        System.out.println(sparse_map_clone.get(4, 5));
+        System.out.println(sparse_map_clone.get(9, 9));
+        // output should be 9 8 3 9 6 null
+    }
+}
